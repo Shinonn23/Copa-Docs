65482888c2c2bb2eb9d53e696dd323bd770e7bd7
Shinonn23 <siwat61719@gmail.com>
2025-05-20 14:59:27 +0700

    feat: create root layout component with metadata and font configuration

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ export default async function RootLayout({ children }) {
       <Head />
       <body>
         <Layout
-          docsRepositoryBase="https://github.com/Shinonn23/Copa-Docs.git"
+          docsRepositoryBase="https://github.com/Shinonn23/Copa-Docs/edit/main/"
           navbar={navbar}
           pageMap={await getPageMap()}
           footer={footer}
